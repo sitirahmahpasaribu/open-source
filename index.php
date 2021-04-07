518662e8f26c812c91e57b82edbea7d1b4491d49
sitirahmahpasaribu <sitirahmah13579@gmail.com>
2021-04-08 00:00:48 +0700

    tugas open source

--- index.php
+++ index.php
@@ -51,8 +51,8 @@
       </nav><!-- .navbar -->
 
       <div class="social-links">
-        <a href="https://www.facebook.com/heiyrbnitsme" class="facebook"><i class="bi bi-facebook"></i></a>
-        <a href="https://www.instagram.com/siti.rahmah1910/" class="instagram"><i class="bi bi-instagram"></i></a>
+        <a href="https://www.instagram.com/heiyrbnitsme/" class="facebook"><i class="bi bi-facebook"></i></a>
+        <a href="https://www.facebook.com/siti.rahmah1910/" class="instagram"><i class="bi bi-instagram"></i></a>
       </div>
 
     </div>
@@ -74,25 +74,29 @@
           <img src="assets/img/saya.jpg" class="img-fluid" alt="">
         </div>
         <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
-          <h3> Sedikit tentang saya </h3>
-          <p class>
-            Hai, perkenalkan saya Siti Rahmah Pasaribu, seorang mahasiswa Politeknik Aceh jurusan Teknologi Informasi.
-          </p>
+          <h3> Siti Rahmah Pasaribu</h3>
+         
           <div class="row">
             <div class="col-lg-6">
               <ul>
-                <li><i class="bi bi-chevron-right"></i> <strong>Birthday:</strong> <span>10 Oktober 1999</span></li>
-                <li><i class="bi bi-chevron-right"></i> <strong>Phone:</strong> <span>+6283157567260</span></li>
-                <li><i class="bi bi-chevron-right"></i> <strong>City:</strong> <span>Banda Aceh, Aceh</span></li>
+                <li><i class="bi bi-chevron-right"></i> <strong>Tanggal Lahir:</strong> <span>10 Oktober 1999</span></li>
+                <li><i class="bi bi-chevron-right"></i> <strong>No.HP:</strong> <span>+6283157567260</span></li>
+                <li><i class="bi bi-chevron-right"></i> <strong>Alamat Saat ini:</strong> <span>Banda Aceh, Aceh</span></li>
               </ul>
             </div>
+
             <div class="col-lg-6">
               <ul>
-                <li><i class="bi bi-chevron-right"></i> <strong>Age:</strong> <span>21</span></li>
-                <li><i class="bi bi-chevron-right"></i> <strong>Degree:</strong> <span>-</span></li>
+                <li><i class="bi bi-chevron-right"></i> <strong>Umur:</strong> <span>21</span></li>
+                <li><i class="bi bi-chevron-right"></i> <strong>Gelar:</strong> <span>-</span></li>
                 <li><i class="bi bi-chevron-right"></i> <strong>Email:</strong> <span>sitirahmah13579@gmail.com</span></li>
               </ul>
             </div>
+
+            <p class>
+            Saat ini saya sedang menempuh pendidikan di Politeknik Aceh sebagai mahasiswa Jurusan Teknologi Informasi, semester 6.
+          </p>
+
           </div>
         </div>
       </div>
@@ -112,23 +116,23 @@
         <div class="col-lg-6">
 
           <div class="progress">
-            <span class="skill">HTML <i class="val">100%</i></span>
+            <span class="skill">HTML <i class="val">80%</i></span>
             <div class="progress-bar-wrap">
-              <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+              <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
             </div>
           </div>
 
           <div class="progress">
-            <span class="skill">CSS <i class="val">90%</i></span>
+            <span class="skill">CSS <i class="val">70%</i></span>
             <div class="progress-bar-wrap">
-              <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+              <div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
             </div>
           </div>
 
           <div class="progress">
-            <span class="skill">JavaScript <i class="val">75%</i></span>
+            <span class="skill">MS Office <i class="val">85%</i></span>
             <div class="progress-bar-wrap">
-              <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+              <div class="progress-bar" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"></div>
             </div>
           </div>
 
@@ -144,18 +148,12 @@
           </div>
 
           <div class="progress">
-            <span class="skill">WordPress/CMS <i class="val">90%</i></span>
+            <span class="skill">WordPress/CMS <i class="val">80%</i></span>
             <div class="progress-bar-wrap">
-              <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+              <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
             </div>
           </div>
 
-          <div class="progress">
-            <span class="skill">Photoshop <i class="val">55%</i></span>
-            <div class="progress-bar-wrap">
-              <div class="progress-bar" role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
-            </div>
-          </div>
 
         </div>
 
@@ -277,11 +275,8 @@
             <i class="bx bx-share-alt"></i>
             <h3>Social Profiles</h3>
             <div class="social-links">
-              <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
-              <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
-              <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
-              <a href="#" class="google-plus"><i class="bi bi-skype"></i></a>
-              <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+              <a href="https://www.facebook.com/siti.rahmah1910/" class="facebook"><i class="bi bi-facebook"></i></a>
+              <a href="https://www.instagram.com/heiyrbnitsme/" class="instagram"><i class="bi bi-instagram"></i></a>
             </div>
           </div>
         </div>
@@ -290,14 +285,14 @@
           <div class="info-box">
             <i class="bx bx-envelope"></i>
             <h3>Email</h3>
-            <p>sitirahmah13579@gamil.com</p>
+            <p>sitirahmah13579@gmail.com</p>
           </div>
         </div>
         <div class="col-md-6 mt-4 d-flex align-items-stretch">
           <div class="info-box">
             <i class="bx bx-phone-call"></i>
             <h3>No Hp</h3>
-            <p>+62 83157567260</p>
+            <p> +62 83157567260</p>
           </div>
         </div>
       </div>
